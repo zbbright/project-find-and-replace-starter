cd982297b87f7b79aa315028ee137b2b9315c54e
tamburkey <petem14@gmail.com>
2020-06-01 17:36:40 -0400

    init

---
+++ main.js
@@ -0,0 +1,28 @@
+let rows = document.querySelectorAll(".row")
+let submitButton = document.querySelector("#submit")
+let colInputElement = document.querySelector("#col-input")
+let rowInputElement = document.querySelector("#row-input")
+let textInputElement = document.querySelector("#text-input")
+let searchSubmit = document.querySelector("#submit-search")
+let searchInputElement = document.querySelector("#search-input")
+let replaceInputElement = document.querySelector("#replace-input")
+
+searchSubmit.addEventListener("click", function (){
+    let searchInputValue = searchInputElement.value
+    let replaceInputValue = replaceInputElement.value
+    for(let i = 0; i < rows.length; i++){
+        let currentRow = rows[i]
+        let cells = currentRow.querySelectorAll(".cell")
+        // for(let j = 0; j < cells.length; j++){
+        //     let currentCell = cells[j]
+        //     if(currentCell.innerHTML === searchInputValue){
+        //         currentCell.innerHTML = replaceInputValue
+        //         return
+        //     }
+        // }
+    }
+    
+})
+
+
+
