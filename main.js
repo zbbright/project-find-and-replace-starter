f5a7b50f28ad375b7a6edb0a37dc6560acae3ab8
davegregg@gmail.com <davegregg@gmail.com>
2020-06-02 11:44:40 -0400

    starter code

--- main.js
+++ main.js
@@ -1,24 +1,21 @@
-let rows = document.querySelectorAll(".row")
-let searchSubmit = document.querySelector("#submit-search")
-let searchInputElement = document.querySelector("#search-input")
-let replaceInputElement = document.querySelector("#replace-input")
+// You should not change the HTML or CSS in this project. Focus on the JavaScript.
 
-searchSubmit.addEventListener("click", function (){
-    let searchInputValue = searchInputElement.value
-    let replaceInputValue = replaceInputElement.value
-    for(let i = 0; i < rows.length; i++){
-        let currentRow = rows[i]
-        let cells = currentRow.querySelectorAll(".cell")
-        // for(let j = 0; j < cells.length; j++){
-        //     let currentCell = cells[j]
-        //     if(currentCell.innerHTML === searchInputValue){
-        //         currentCell.innerHTML = replaceInputValue
-        //         return
-        //     }
-        // }
-    }
-    
-})
+const findInput = document.querySelector(".find-input")
+const replaceInput = document.querySelector(".replace-input")
+const replaceButton = document.querySelector(".replace-all-button")
 
+// The following variable holds your OUTER ARRAY of row elements.
+// Later you will need an OUTER LOOP to loop over the individual elements within
+// this array.
+const rowElements = document.querySelectorAll(".row")
 
+// When you call the function belwo, it will get and return an INNER ARRAY
+// containing the cell elements for a given row.
+// Call this function from WITHIN your row elements loop. Then you will, in turn,
+// need to loop over the resulting cell elements. But where should this whole
+// NESTED LOOP go? Think through the user's experience: when should WHAT happen? 
+function getCellElements (currentRowElement) {
+    return currentRowElement.querySelectorAll(".cell")
+}
 
+// YOUR CODE GOES HERE
