a5fa6bc969d45db5e90b20e19e1b6f280d56df88
davegregg@gmail.com <davegregg@gmail.com>
2020-06-02 12:04:27 -0400

    Add notes, clean up HTML a bit

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
-// You should not change the HTML or CSS in this project. Focus on the JavaScript.
+// You should NOT change the HTML or CSS in this project (at least until you reach
+// the bonus objectives). Focus on the JavaScript.
 
 const findInput = document.querySelector(".find-input")
 const replaceInput = document.querySelector(".replace-input")
@@ -18,4 +19,20 @@ function getCellElements (currentRowElement) {
     return currentRowElement.querySelectorAll(".cell")
 }
 
+
 // YOUR CODE GOES HERE
+
+
+// One last thing: dedicate very careful attention to using variables and
+// naming them accurately.
+// And when you change the value you are assigning to a variable, don't
+// forget to consider changing the name to reflect the change you made! It
+// is very easy to get confused when you are working inside NESTED LOOPS.
+// The best of us do. And unnecessary confusion during the process of 
+// developing your code means wasted time.
+//
+// The time-cost of structuring and naming things well is FAR less than the
+// time-cost of ignoring the quality and readability of your code.
+//
+// You can, of course, remove any comments in this starter project once
+// you have read them, if you prefer.
